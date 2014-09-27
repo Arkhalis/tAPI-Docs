318951efb965a167dc8cb130a742ad6980304e4a
Arkhalis <arkhalis0@yahoo.com>
2014-09-27 15:00:22 -0500

    Added tile.php

--- docs/nav.php
+++ docs/nav.php
@@ -3,8 +3,9 @@
 		<a href="#" class="dropdown-toggle" data-toggle="dropdown">JSON <b class="caret"></b></a>
 		<ul class="dropdown-menu">
 			<?php echo($GLOBALS["gDoc"] == "modinfo" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=modinfo">ModInfo</a></li>
-			<?php echo($GLOBALS["gDoc"] == "item" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=item">Item</a></li>
+			<?php echo($GLOBALS["gDoc"] == "item" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=item">Items</a></li>
 			<?php echo($GLOBALS["gDoc"] == "npc" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=npc">NPCs</a></li>
+			<?php echo($GLOBALS["gDoc"] == "tile" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=tile">Tiles</a></li>
 		</ul>
 	</li>
 </ul>
