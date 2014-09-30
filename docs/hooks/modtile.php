898065f53d7d3226d4b0aed3ce746b2677ddb36e
Arkhalis <arkhalis0@yahoo.com>
2014-09-29 21:50:43 -0500

    Creation of hook.php and CS ddmenu

---
+++ docs/hooks/modtile.php
@@ -0,0 +1,98 @@
+<?php
+
+$GLOBALS["propsInfo"] = array(
+	"header" => "ModInfo",
+	"info" => "Main mod configuration file. Required by every mod.",
+	"tags" => array(
+		array(
+			"tag" => "internal",
+			"name" => "Internal"
+		),
+		array(
+			"tag" => "info",
+			"name" => "Informative"
+		)
+	)
+);
+
+$GLOBALS["props"] = array(
+	array(
+		"tags" => array("internal"),
+		"name" => "internalName",
+		"type" => "string",
+		"text" => "The name the mod is known as internally, also the namespace used by the mod in code.",
+		"drop" => "
+			<div>This property has to be unique between all mods.</div>
+			<div class=\"bs-callout bs-callout-warning\">
+				Don't use any names that might be used in other namespaces, for example:<br />
+				<ul><li>Terraria</li><li>TAPI</li><li>LitJson</li><li>System</li></ul>
+			</div>
+		"
+	),
+	array(
+		"tags" => array("info"),
+		"name" => "displayName",
+		"type" => "string",
+		"text" => "The name visible in the mods menu."
+	),
+	array(
+		"tags" => array("info"),
+		"name" => "author",
+		"type" => "string",
+		"text" => "Mod author."
+	),
+	array(
+		"tags" => array("info"),
+		"name" => "version",
+		"type" => array("string","int[1-4]"),
+		"text" => "The name the mod is known as internally, also the namespace used by the mod in code.",
+		"default" => "<code>[1,0,0,0]</code>",
+		"drop" => "
+			<div class=\"bs-example\">
+				<code>\"version\": \"r1\"</code><br />
+				<code>\"version\": [1,0,0,0]</code>
+			</div>
+		"
+	),
+	array(
+		"tags" => array("internal"),
+		"name" => "includePDB",
+		"type" => "bool",
+		"text" => "Whether the mod packer should create a debug PDB file.",
+		"default" => "<code>false</code>",
+		"drop" => "
+			<div>The PDB file allows tAPI to display line numbers on which errors happen.</div>
+		"
+	),
+	array(
+		"tags" => array("internal"),
+		"name" => "warnOnReload",
+		"type" => "bool",
+		"text" => "Determines whether it's unsafe to reload the mod and the game should be restarted instead.",
+		"default" => "<code>false</code>",
+		"drop" => "
+			<div>Some mods that allow to be used as APIs can cause problems when reloaded. Set this property to mark such mods.</div>
+		"
+	),
+	array(
+		"tags" => array("internal"),
+		"name" => "modReferences",
+		"type" => "string[?]",
+		"text" => "List of all mod dependencies.",
+		"drop" => "
+			<div class=\"myDropdownDiv\">All the names are mod <code>internalName</code> properties.</div>
+		"
+	),
+	array(
+		"tags" => array("internal"),
+		"name" => "dllReferences",
+		"type" => "string[?]",
+		"text" => "List of all DLL dependencies.",
+		"drop" => "
+			
+		",
+		"warning" => true
+	)
+);
+
+?>
--- docs/nav.php
+++ docs/nav.php
@@ -2,10 +2,31 @@
 	<?php echo("<li class=\"dropdown".($GLOBALS["gMode"] == "properties" ? " active" : "")."\">"); ?>
 		<a href="#" class="dropdown-toggle" data-toggle="dropdown">JSON <b class="caret"></b></a>
 		<ul class="dropdown-menu">
-			<?php echo($GLOBALS["gDoc"] == "modinfo" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=modinfo">ModInfo</a></li>
-			<?php echo($GLOBALS["gDoc"] == "item" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=item">Items</a></li>
-			<?php echo($GLOBALS["gDoc"] == "npc" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=npc">NPCs</a></li>
-			<?php echo($GLOBALS["gDoc"] == "tile" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=tile">Tiles</a></li>
+			<?php echo($GLOBALS["jsonDoc"] == "modinfo" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=modinfo">ModInfo</a></li>
+			<?php echo($GLOBALS["jsonDoc"] == "buff" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=buff">Buffs</a></li>
+			<?php echo($GLOBALS["jsonDoc"] == "item" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=item">Items</a></li>
+			<?php echo($GLOBALS["jsonDoc"] == "npc" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=npc">NPCs</a></li>
+			<?php echo($GLOBALS["jsonDoc"] == "prefix" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=prefix">Prefixes</a></li>
+			<?php echo($GLOBALS["jsonDoc"] == "projectile" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=projectile">Projectiles</a></li>
+			<?php echo($GLOBALS["jsonDoc"] == "tile" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=tile">Tiles</a></li>
+			<?php echo($GLOBALS["jsonDoc"] == "wall" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=properties&doc=wall">Walls</a></li>
+		</ul>
+	</li>
+	<?php echo("<li class=\"dropdown".($GLOBALS["gMode"] == "hooks" ? " active" : "")."\">"); ?>
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown">CS <b class="caret"></b></a>
+		<ul class="dropdown-menu">
+			<?php echo($GLOBALS["csDoc"] == "modbase" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=hooks&doc=modbase">ModBase</a></li>
+			<?php echo($GLOBALS["csDoc"] == "modbuff" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=hooks&doc=modbuff">ModBuff</a></li>
+			<?php echo($GLOBALS["csDoc"] == "modentity" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=hooks&doc=modentity">ModEntity</a></li>
+			<?php echo($GLOBALS["csDoc"] == "modinterface" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=hooks&doc=modinterface">ModInterface</a></li>
+			<?php echo($GLOBALS["csDoc"] == "moditem" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=hooks&doc=moditem">ModItem</a></li>
+			<?php echo($GLOBALS["csDoc"] == "modnpc" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=hooks&doc=modnpc">ModNPC</a></li>
+			<?php echo($GLOBALS["csDoc"] == "modplayer" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=hooks&doc=modplayer">ModPlayer</a></li>
+			<?php echo($GLOBALS["csDoc"] == "modprefix" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=hooks&doc=modprefix">ModPrefix</a></li>
+			<?php echo($GLOBALS["csDoc"] == "modprojectile" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=hooks&doc=modprojectile">ModProjectile</a></li>
+			<?php echo($GLOBALS["csDoc"] == "modtile" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=hooks&doc=modtile">ModTile</a></li>
+			<?php echo($GLOBALS["csDoc"] == "modtiletype" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=hooks&doc=modtiletype">ModTileType</a></li>
+			<?php echo($GLOBALS["csDoc"] == "modworld" ? "<li class=\"active\">" : "<li>"); ?><a href="./?mode=hooks&doc=modworld">ModWorld</a></li>
 		</ul>
 	</li>
 </ul>
